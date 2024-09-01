4e3d8988f62ebc121f2d74617d5988c6d24ad829
Siddarth Menon <siddarth.menon@agilepoint.com>
2024-09-01 18:22:00 +0530

    added slider and textfield

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,69 @@
+import { createTheme, ThemeProvider } from '@mui/material'
+import { NumberInputComponent, SliderComponent } from './components/slider'
 import './App.css'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4d9500',
+    },
+  },
+  typography: {
+    allVariants: {
+      color: '#eeeeee',
+    },
+  },
+  components: {
+    MuiInputBase: {
+      styleOverrides: {
+        input: {
+          color: '#949494',
+        },
+      },
+    },
+    MuiInputAdornment : {
+      styleOverrides: {
+        root: {
+          color: '#949494'
+        }
+      }
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          color: '#949494'
+        }
+      }
+    },
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          color: '#949494'
+        }
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        notchedOutline: {
+          borderColor: '#949494'
+        }
+      }
+    }
+  }
+})
 
 function App() {
 
   return (
-    <h1 className="text-4xl font-bold underline">
-      Hello world!
-    </h1>
+    <ThemeProvider theme={theme}>
+      <>
+        <h1 className="text-4xl font-bold underline">
+          Hello world!
+        </h1>
+        <SliderComponent />
+        <NumberInputComponent />
+      </>
+    </ThemeProvider>
   )
 }
 
